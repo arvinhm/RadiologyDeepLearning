a8be9d96ce64cb17567b50f4e34da2a3ad9d6c02
Arvin Haj-Mirzaian <45150683+arvinhm@users.noreply.github.com>
2024-06-15 11:22:05 -0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -411,10 +411,10 @@ class MBConv3d(nn.Module):
         return x
 
 class EfficientNet3D(nn.Module):
-    def __init__(self, num_classes=1000):
+    def __init__(self, in_channels, num_classes=1000):
         super(EfficientNet3D, self).__init__()
         self.features = nn.Sequential(OrderedDict([
-            ('conv1', Conv3dSame(3, 32, kernel_size=3, stride=2)),
+            ('conv1', Conv3dSame(in_channels, 32, kernel_size=3, stride=2)),
             ('bn1', nn.BatchNorm3d(32)),
             ('swish', nn.SiLU()),
             ('mbconv1', MBConv3d(32, 16, 1, 1)),
@@ -441,12 +441,13 @@ class EfficientNet3D(nn.Module):
         return x
 
 def efficientnet3d_b0(in_channels, num_classes):
-    model = EfficientNet3D(num_classes=num_classes)
+    model = EfficientNet3D(in_channels=in_channels, num_classes=num_classes)
     return model
 
 def efficientnet3d_b7(in_channels, num_classes):
-    model = EfficientNet3D(num_classes=num_classes)
+    model = EfficientNet3D(in_channels=in_channels, num_classes=num_classes)
     return model
+
 
 
 # VGG3D
@@ -600,46 +601,355 @@ def resnext3d(in_channels, num_classes):
 
 
 # ViT3D
+import torch
+import torch.nn as nn
+from torch.nn import Conv3d, Dropout, LayerNorm
+from torch.utils.data import DataLoader, Subset
+import copy
 
-class ViT3D(nn.Module):
-    def __init__(self, in_channels, num_classes, patch_size=(16, 16, 16)):
-        super(ViT3D, self).__init__()
-        self.model = timm.create_model('vit_base_patch16_224', pretrained=True)
-        self.model.patch_embed.proj = nn.Conv3d(in_channels, self.model.embed_dim, kernel_size=patch_size, stride=patch_size)
-        num_patches = (112 // patch_size[0]) * (112 // patch_size[1]) * (112 // patch_size[2])
-        self.model.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, self.model.embed_dim))
-        self.model.head = nn.Linear(self.model.head.in_features, num_classes)
-        
-        # Update all layers to support 3D inputs
-        for block in self.model.blocks:
-            block.attn.qkv = nn.Linear(block.attn.qkv.in_features, block.attn.qkv.out_features)
-            block.attn.proj = nn.Linear(block.attn.proj.in_features, block.attn.proj.out_features)
-            block.mlp.fc1 = nn.Linear(block.mlp.fc1.in_features, block.mlp.fc1.out_features)
-            block.mlp.fc2 = nn.Linear(block.mlp.fc2.in_features, block.mlp.fc2.out_features)
+class CNNEncoder(nn.Module):
+    def __init__(self, config, n_channels=2):
+        super(CNNEncoder, self).__init__()
+        self.n_channels = n_channels
+        decoder_channels = config['decoder_channels']
+        encoder_channels = config['encoder_channels']
+        self.down_num = config['down_num']
+        self.inc = Conv3dReLU(n_channels, encoder_channels[0], kernel_size=3, padding=1)
+        self.down1 = Down(encoder_channels[0], encoder_channels[1])
+        self.down2 = Down(encoder_channels[1], encoder_channels[2])
+        self.width = encoder_channels[-1]
 
     def forward(self, x):
-        B, C, D, H, W = x.shape  # Expecting a 5D input
-        x = self.model.patch_embed.proj(x).flatten(2).transpose(1, 2)  # Adjust the patch embedding layer
-        cls_tokens = self.model.cls_token.expand(B, -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = self.model.pos_drop(x + self.model.pos_embed)
-        for block in self.model.blocks:
-            x = block(x)
-        x = self.model.norm(x)
-        return self.model.head(x[:, 0])
+        features = []
+        x1 = self.inc(x)
+        features.append(x1)
+        x2 = self.down1(x1)
+        features.append(x2)
+        feats = self.down2(x2)
+        features.append(feats)
+        feats_down = feats
+        for i in range(self.down_num):
+            feats_down = nn.MaxPool3d(2)(feats_down)
+            features.append(feats_down)
+        return feats, features[::-1]
 
-def vit3d(in_channels, num_classes):
-    return ViT3D(in_channels, num_classes)
+class ViTVNet(nn.Module):
+    def __init__(self, config, img_size=(16, 128, 128), vis=False):
+        super(ViTVNet, self).__init__()
+        self.transformer = Transformer(config, img_size, vis)
+        self.decoder = DecoderCup(config, img_size)
+        self.reg_head = RegistrationHead(
+            in_channels=config['decoder_channels'][-1],
+            out_channels=config['n_dims'],
+            kernel_size=3,
+        )
+        self.spatial_trans = SpatialTransformer(img_size)
+        self.config = config
+        #self.integrate = VecInt(img_size, int_steps)
+    def forward(self, x):
+        source = x[:,0:1,:,:]
+        x, attn_weights, features = self.transformer(x)  # (B, n_patch, hidden)
+        x = self.decoder(x, features)
+        flow = self.reg_head(x)
+        #flow = self.integrate(flow)
+        out = self.spatial_trans(source, flow)
+        return out, flow
 
+class Transformer(nn.Module):
+    def __init__(self, config, img_size, vis):
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config, img_size=img_size)
+        self.encoder = Encoder(config, vis)
 
+    def forward(self, input_ids):
+        embedding_output, features = self.embeddings(input_ids)
+        encoded, attn_weights = self.encoder(embedding_output)  # (B, n_patch, hidden)
+        return encoded, attn_weights, features
 
+class Encoder(nn.Module):
+    def __init__(self, config, vis):
+        super(Encoder, self).__init__()
+        self.vis = vis
+        self.layer = nn.ModuleList()
+        self.encoder_norm = LayerNorm(config["hidden_size"], eps=1e-6)
+        for _ in range(config["transformer"]["num_layers"]):
+            layer = Block(config, vis)
+            self.layer.append(copy.deepcopy(layer))
 
-# Helper function to list available models in timm
-def list_available_timm_models():
-    model_names = timm.list_models(pretrained=True)
-    print("Available models in timm:")
-    for name in model_names:
-        print(name)
+    def forward(self, hidden_states):
+        attn_weights = []
+        for layer_block in self.layer:
+            hidden_states, weights = layer_block(hidden_states)
+            if self.vis:
+                attn_weights.append(weights)
+        encoded = self.encoder_norm(hidden_states)
+        return encoded, attn_weights
 
-if __name__ == "__main__":
-    list_available_timm_models()
+class Block(nn.Module):
+    def __init__(self, config, vis):
+        super(Block, self).__init__()
+        self.hidden_size = config["hidden_size"]
+        self.attention_norm = LayerNorm(config["hidden_size"], eps=1e-6)
+        self.ffn_norm = LayerNorm(config["hidden_size"], eps=1e-6)
+        self.ffn = Mlp(config)
+        self.attn = Attention(config, vis)
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x, weights = self.attn(x)
+        x = x + h
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x, weights
+
+class Attention(nn.Module):
+    def __init__(self, config, vis):
+        super(Attention, self).__init__()
+        self.vis = vis
+        self.num_attention_heads = config["transformer"]["num_heads"]
+        self.attention_head_size = int(config["hidden_size"] / self.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = Linear(config["hidden_size"], self.all_head_size)
+        self.key = Linear(config["hidden_size"], self.all_head_size)
+        self.value = Linear(config["hidden_size"], self.all_head_size)
+
+        self.out = Linear(config["hidden_size"], config["hidden_size"])
+        self.attn_dropout = Dropout(config["transformer"]["attention_dropout_rate"])
+        self.proj_dropout = Dropout(config["transformer"]["attention_dropout_rate"])
+
+        self.softmax = Softmax(dim=-1)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = self.softmax(attention_scores)
+        weights = attention_probs if self.vis else None
+        attention_probs = self.attn_dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+        attention_output = self.proj_dropout(attention_output)
+        return attention_output, weights
+
+class Mlp(nn.Module):
+    def __init__(self, config):
+        super(Mlp, self).__init__()
+        self.fc1 = Linear(config["hidden_size"], config["transformer"]["mlp_dim"])
+        self.fc2 = Linear(config["transformer"]["mlp_dim"], config["hidden_size"])
+        self.act_fn = torch.nn.functional.gelu
+        self.dropout = Dropout(config["transformer"]["dropout_rate"])
+        self._init_weights()
+
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias, std=1e-6)
+        nn.init.normal_(self.fc2.bias, std=1e-6)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+class Embeddings(nn.Module):
+    def __init__(self, config, img_size):
+        super(Embeddings, self).__init__()
+        self.config = config
+        down_factor = config["down_factor"]
+        patch_size = _triple(config["patches"]["size"])
+        n_patches = int((img_size[0] // 2 ** down_factor // patch_size[0]) *
+                        (img_size[1] // 2 ** down_factor // patch_size[1]) *
+                        (img_size[2] // 2 ** down_factor // patch_size[2]))
+        self.hybrid_model = CNNEncoder(config, n_channels=2)
+        in_channels = config["encoder_channels"][-1]
+        self.patch_embeddings = Conv3d(in_channels=in_channels,
+                                       out_channels=config["hidden_size"],
+                                       kernel_size=patch_size,
+                                       stride=patch_size)
+        self.position_embeddings = nn.Parameter(torch.zeros(1, n_patches, config["hidden_size"]))
+        self.dropout = Dropout(config["transformer"]["dropout_rate"])
+
+    def forward(self, x):
+        x, features = self.hybrid_model(x)
+        x = self.patch_embeddings(x)
+        x = x.flatten(2)
+        x = x.transpose(-1, -2)
+        embeddings = x + self.position_embeddings[:, :x.size(1), :]
+        embeddings = self.dropout(embeddings)
+        return embeddings, features
+
+class Conv3dReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, padding=0, stride=1, use_batchnorm=True):
+        conv = nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=not use_batchnorm)
+        relu = nn.ReLU(inplace=True)
+        bn = nn.BatchNorm3d(out_channels)
+        super(Conv3dReLU, self).__init__(conv, bn, relu)
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, skip_channels=0, use_batchnorm=True):
+        super().__init__()
+        self.conv1 = Conv3dReLU(in_channels + skip_channels, out_channels, kernel_size=3, padding=1, use_batchnorm=use_batchnorm)
+        self.conv2 = Conv3dReLU(out_channels, out_channels, kernel_size=3, padding=1, use_batchnorm=use_batchnorm)
+        self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+
+    def forward(self, x, skip=None):
+        x = self.up(x)
+        if skip is not None:
+            x = torch.cat([x, skip], dim=1)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+class DecoderCup(nn.Module):
+    def __init__(self, config, img_size):
+        super().__init__()
+        self.config = config
+        self.down_factor = config["down_factor"]
+        head_channels = config["conv_first_channel"]
+        self.img_size = img_size
+        self.conv_more = Conv3dReLU(config["hidden_size"], head_channels, kernel_size=3, padding=1, use_batchnorm=True)
+        decoder_channels = config["decoder_channels"]
+        in_channels = [head_channels] + list(decoder_channels[:-1])
+        out_channels = decoder_channels
+        self.patch_size = _triple(config["patches"]["size"])
+        skip_channels = self.config["skip_channels"]
+        blocks = [DecoderBlock(in_ch, out_ch, sk_ch) for in_ch, out_ch, sk_ch in zip(in_channels, out_channels, skip_channels)]
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, hidden_states, features=None):
+        B, n_patch, hidden = hidden_states.size()  # reshape from (B, n_patch, hidden) to (B, h, w, hidden)
+        l, h, w = (self.img_size[0] // 2 ** self.down_factor // self.patch_size[0], 
+                   self.img_size[1] // 2 ** self.down_factor // self.patch_size[1], 
+                   self.img_size[2] // 2 ** self.down_factor // self.patch_size[2])
+        x = hidden_states.permute(0, 2, 1)
+        x = x.contiguous().view(B, hidden, l, h, w)
+        x = self.conv_more(x)
+        for i, decoder_block in enumerate(self.blocks):
+            if features is not None:
+                skip = features[i] if (i < self.config["n_skip"]) else None
+                if skip is not None:
+                    print(f"Shape of skip {i}: {skip.shape}")
+            else:
+                skip = None
+            x = decoder_block(x, skip=skip)
+            print(f"Shape of x after decoder block {i}: {x.shape}")
+        return x
+
+class RegistrationHead(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=1):
+        conv3d = nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        conv3d.weight = nn.Parameter(dist.Normal(0, 1e-5).sample(conv3d.weight.shape))
+        conv3d.bias = nn.Parameter(torch.zeros(conv3d.bias.shape))
+        super().__init__(conv3d)
+
+class SpatialTransformer(nn.Module):
+    def __init__(self, size, mode='bilinear'):
+        super().__init__()
+        self.mode = mode
+        vectors = [torch.arange(0, s) for s in size]
+        grids = torch.meshgrid(vectors)
+        grid = torch.stack(grids)
+        grid = torch.unsqueeze(grid, 0)
+        grid = grid.type(torch.FloatTensor)
+        self.register_buffer('grid', grid)
+
+    def forward(self, src, flow):
+        new_locs = self.grid + flow
+        shape = flow.shape[2:]
+        for i in range(len(shape)):
+            new_locs[:, i, ...] = 2 * (new_locs[:, i, ...] / (shape[i] - 1) - 0.5)
+        if len(shape) == 2:
+            new_locs = new_locs.permute(0, 2, 3, 1)
+            new_locs = new_locs[..., [1, 0]]
+        elif len(shape) == 3:
+            new_locs = new_locs.permute(0, 2, 3, 4, 1)
+            new_locs = new_locs[..., [2, 1, 0]]
+        return nn.functional.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
+
+class VecInt(nn.Module):
+    def __init__(self, inshape, nsteps):
+        super().__init__()
+        assert nsteps >= 0, 'nsteps should be >= 0, found: %d' % nsteps
+        self.nsteps = nsteps
+        self.scale = 1.0 / (2 ** self.nsteps)
+        self.transformer = SpatialTransformer(inshape)
+
+    def forward(self, vec):
+        vec = vec * self.scale
+        for _ in range(self.nsteps):
+            vec = vec + self.transformer(vec, vec)
+        return vec
+
+# Configuration for ViTVNet
+config = {
+    "hidden_size": 768,
+    "transformer": {
+        "num_heads": 12,
+        "attention_dropout_rate": 0.1,
+        "num_layers": 12,
+        "mlp_dim": 3072,
+        "dropout_rate": 0.1,
+    },
+    "down_factor": 4,
+    "conv_first_channel": 64,
+    "decoder_channels": [128, 64, 32],
+    "encoder_channels": [64, 128, 256],
+    "skip_channels": [256, 128, 64],
+    "n_skip": 3,
+    "patches": {"size": [2, 4, 4]},
+    "n_dims": 3,
+    "down_num": 2
+}
+
+# Initialize the model with configuration
+def create_vit_vnet():
+    return ViTVNet(config)
